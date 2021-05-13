2eff021796fcc4ea4d1b8f99638c383a6f7c597c
nigelreign <zulunigelb@gmail.com>
2021-05-13 09:13:30 +0200

    adding rest-auth urls

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
     # this will map the path to api basic urls which will have all other endpoint links
     path('', include('api_basic.urls'))
 ]
